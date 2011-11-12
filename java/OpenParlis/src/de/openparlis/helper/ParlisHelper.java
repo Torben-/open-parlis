4f472bf792d3cd9cce7bb017d149b6f255832266
torben.hedderich@gmail.com <Torben-@users.noreply.github.com>
2011-11-12 12:53:37 +0000

    First steps DocList Parser ...

--- java/OpenParlis/src/de/openparlis/helper/ParlisHelper.java
+++ java/OpenParlis/src/de/openparlis/helper/ParlisHelper.java
@@ -7,12 +7,26 @@ import de.openparlis.data.DocumentId;
 
 public class ParlisHelper {
 	
-	static final String PARLIS_DOCUMENT_URL = "http://stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27{DOCUMENT_ID}%27";
+	static final int PARLIS_DOCS_PER_PAGE = 20;
+	static final String PARLIS_BASE_URL = "http://stvv.frankfurt.de/PARLISLINK/";
+	static final String PARLIS_DOCUMENT_URL = "DDW?W=DOK_NAME=%27{DOCUMENT_ID}%27";
+	static final String PARLIS_DOCLIST_URL = "SDW?W%3DORDER+BY+DATUM/Descend%26M%3D{PAGE_NUM}%26R%3DY";
 
-	static public URL createParlisUrl(DocumentId documentId) {
+	static public URL createParlisDocumentUrl(DocumentId documentId) {
 		try {
-			String url = PARLIS_DOCUMENT_URL;
+			String url = PARLIS_BASE_URL + PARLIS_DOCUMENT_URL;
 			url = url.replaceAll("\\{DOCUMENT_ID\\}", documentId.toString());
+			return new URL(url);
+		} catch (MalformedURLException ex) {
+			return null;
+		}
+	}
+	
+	static public URL createParlisDocListUrl(int page) {
+		try {
+			int pageNum = (PARLIS_DOCS_PER_PAGE * page) + 1;
+			String url = PARLIS_BASE_URL + PARLIS_DOCLIST_URL;
+			url = url.replaceAll("\\{PAGE_NUM\\}", Integer.toString(pageNum));
 			return new URL(url);
 		} catch (MalformedURLException ex) {
 			return null;
